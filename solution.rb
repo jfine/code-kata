9482809c873dfed3e0b91f24a9e16989c1793842
Jared Fine <jared@jfine.org>
2016-02-14 12:48:58 -0500

    I got too clever...

--- solution.rb
+++ solution.rb
@@ -1,1 +1,15 @@
-# Add your solution here
+NUMBERS = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6,
+  seven: 7, eight: 8, nine: 9 }
+OPERATORS = { plus: :+, minus: :-, times: :*, divided_by: :/ }
+
+NUMBERS.each do |name, number|
+  define_method(name) do |operation = nil|
+    operation.nil? ? number : number.send(*operation)
+  end
+end
+
+OPERATORS.each do |name, operator|
+  define_method(name) do |number|
+    [operator, number]
+  end
+end
--- tests.rb
+++ tests.rb
@@ -1,11 +1,14 @@
 require_relative 'solution'
 
-
 describe 'solution' do
   it 'passes these tests' do
     expect(seven(times(five))).to eq 35
     expect(four(plus(nine))).to eq 13
     expect(eight(minus(three))).to eq 5
     expect(six(divided_by(two))).to eq 3
+  end
+
+  it 'is chainable' do
+    expect(five(times(five(times(five))))).to eq 125
   end
 end
